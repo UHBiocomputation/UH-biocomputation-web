4813ed1adb20cdc3a5f45c18583de351aa1e4e3e
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2015-09-09 17:20:28 +0100

    Initial pelican setup.

---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Dr. Volker Steuber'
+SITENAME = u'UH Biocomputation Group'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Disclaimer', 'http://homepages.stca.herts.ac.uk/~nngroup/disclaimer.php'),
+        )
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
