184631544149ed862c3c4c57b1c80b7395bb6d80
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2015-09-18 10:05:16 +0100

    Updated siteurl.

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 #SITEURL = 'http://homepages.stca.herts.ac.uk/~nngroup/'
-SITEURL = 'http://ankursinha.in/uh-biocom-demo'
+SITEURL = 'http://biocomputation.herts.ac.uk'
 
 FEED_DOMAIN = SITEURL
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
