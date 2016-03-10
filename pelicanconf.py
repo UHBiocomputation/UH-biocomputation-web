01887ecc471c2252ed3d0990ce726246b0cc526d
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2016-03-10 11:52:15 +0000

    Replace open position with latest post on home page

--- pelicanconf.py
+++ pelicanconf.py
@@ -63,5 +63,5 @@ TAG_CLOUD_SORTING = 'random'
 DISPLAY_CATEGORIES_ON_MENU = False
 
 # Minor customization to make some info appear on our home page
-CUSTOM_POST_ON_HOME_PAGE = 'Please check the <a href="blog_index.html">news page</a> for information on an open PhD studentship that is currently available at the Biocomputation group.'
-LATEST_ARTICLE_ON_HOME_PAGE = False
+#CUSTOM_POST_ON_HOME_PAGE = 'Please check the <a href="blog_index.html">news page</a> for information on an open PhD studentship that is currently available at the Biocomputation group.'
+LATEST_ARTICLE_ON_HOME_PAGE = True
