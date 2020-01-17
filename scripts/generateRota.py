a785e548ff9628684acddf619661fac90f03780b
edd26 <ed18abc@herts.ac.uk>
2020-01-17 13:41:30 +0000

    Update of the JC rota

--- scripts/generateRota.py
+++ scripts/generateRota.py
@@ -66,7 +66,7 @@ class generateRota:
         # print("A week is: {}".format(self.a_week))
         self.tz = pytz.timezone("Europe/London")
         # The date this rota starts at
-        self.start_date = date(2020, 1, 24)
+        self.start_date = date(2020, 1, 31)
         # defaults
         self.year = str(datetime.now().year)
         
