5166893a778e0aad23d1f565a4f323021d68aae6
Eleonora Bernasconi <ely97ber@gmail.com>
2024-11-06 14:22:19 +0000

    Update start of JC

--- scripts/generateRota.py
+++ scripts/generateRota.py
@@ -69,7 +69,7 @@ class generateRota:
 
         # ===-===-===-===-===-===-===-===-===-===-===-===-
         # TODO this should be changed to be loaded from the rota csv files
-        self.start_date = date(2024, 10, 18)
+        self.start_date = date(2024, 10, 25)
         # ===-===-===-===-===-===-===-===-===-===-===-===-
 
         # defaults
